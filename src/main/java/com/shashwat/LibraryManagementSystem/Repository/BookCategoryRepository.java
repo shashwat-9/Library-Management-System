d6f3d86abc757c578548940ddd0eed76847951a3
Shashwat Mishra <shashwatmishra91@gmail.com>
2022-08-12 16:54:25 +0530

    few changes made

--- src/main/java/com/shashwat/LibraryManagementSystem/Repository/BookCategoryRepository.java
+++ src/main/java/com/shashwat/LibraryManagementSystem/Repository/BookCategoryRepository.java
@@ -3,13 +3,13 @@ package com.shashwat.LibraryManagementSystem.Repository;
 import com.shashwat.LibraryManagementSystem.models.Books.BookCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface BookCategoryRepository extends JpaRepository<BookCategory, String> {
 
     BookCategory findById(int id);
 
     BookCategory findByTitle(String title);
+
+    Boolean existsByTitleIgnoreCase(String title);
 
     boolean existsById(int id);
 
