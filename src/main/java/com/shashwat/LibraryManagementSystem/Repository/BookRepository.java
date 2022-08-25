f332af760d62767a4eb16caf4d58b704a6575ca5
Shashwat Mishra <shashwatmishra91@gmail.com>
2022-08-25 16:41:31 +0530

    New Query Methods added

--- src/main/java/com/shashwat/LibraryManagementSystem/Repository/BookRepository.java
+++ src/main/java/com/shashwat/LibraryManagementSystem/Repository/BookRepository.java
@@ -3,10 +3,11 @@ package com.shashwat.LibraryManagementSystem.Repository;
 import com.shashwat.LibraryManagementSystem.models.Books.Author;
 import com.shashwat.LibraryManagementSystem.models.Books.Book;
 import com.shashwat.LibraryManagementSystem.models.Books.BookCategory;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 public interface BookRepository extends JpaRepository<Book, Integer> {
@@ -14,4 +15,12 @@ public interface BookRepository extends JpaRepository<Book, Integer> {
     List<Book> findBookByTitle(String title);
 //    List<Book> findBooksByAuthorsList(List<Author> authors);
     List<Book> findBooksByBookCategory(BookCategory bookCategory);
+
+    Book findById(int id);
+
+    @Transactional
+    @Modifying
+    @Query("update Book book set book.availableCopies =:change where book.id =:id")
+    void changeAvailableCopies(int id, int change);
+
 }
