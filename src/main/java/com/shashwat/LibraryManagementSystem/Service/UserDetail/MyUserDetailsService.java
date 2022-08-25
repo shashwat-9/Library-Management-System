d7d83af8a68ab1b5f8a1c4f4715b5d9033e0346b
Shashwat Mishra <shashwatmishra91@gmail.com>
2022-08-25 16:51:43 +0530

    MyUserDetailsService added for database authentication

---
+++ src/main/java/com/shashwat/LibraryManagementSystem/Service/UserDetail/MyUserDetailsService.java
@@ -0,0 +1,20 @@
+package com.shashwat.LibraryManagementSystem.Service.UserDetail;
+
+import com.shashwat.LibraryManagementSystem.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username);
+    }
+}
